11b3dc50e1c55795948fc14ba0e74c45ec3ec549
cu3x <gk@indiko.ch>
2024-12-03 08:53:09 +0100

    wip: tab commands

---
+++ src/Indiko.Maui.Controls.Chat/Platforms/iOS/UiViewExtensions.cs
@@ -0,0 +1,19 @@
+﻿using UIKit;
+
+namespace Indiko.Maui.Controls.Chat.Platforms.iOS;
+internal static class UiViewExtensions
+{
+    public static void AnimateFade(this UIView view)
+    {
+        UIView.Animate(0.2, 0, UIViewAnimationOptions.CurveEaseInOut, () =>
+        {
+            view.Alpha = 0.7f;
+        }, () =>
+        {
+            UIView.Animate(0.2, 0, UIViewAnimationOptions.CurveEaseInOut, () =>
+            {
+                view.Alpha = 1.0f;
+            }, null);
+        });
+    }
+}
